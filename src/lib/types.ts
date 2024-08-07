5868a203e42cfe66b9ac69262e10f6084963f30d
amenowski <contactserus@gmail.com>
2024-08-07 17:46:52 +0200

    feat: create type for post, like and comment

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -8,3 +8,29 @@ export type TLogin = {
   email: string;
   password: string;
 };
+
+export type TPost = {
+  id: number;
+  createdAt?: Date;
+  userId: string;
+  content: string;
+  tags: string[];
+  likes: TLike[];
+  images: File[] | string[];
+  comments: TComment[];
+};
+
+export type TLike = {
+  id: number;
+  userId: string;
+  username: string;
+  postId: number;
+};
+
+export type TComment = {
+  id: number;
+  userId: string;
+  username: string;
+  postId: string;
+  content: string;
+};
